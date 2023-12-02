8bfe800576d441701c6a864c8acee1aa56c3af73
Nguyen Dai <ngdai3042004@gmail.com>
2023-12-02 12:37:42 +0700

    push

--- Assets/Scripts/DbRequestManager.cs
+++ Assets/Scripts/DbRequestManager.cs
@@ -14,7 +14,7 @@ public class DbRequestManager : MonoBehaviour
         {
             if(instance == null)
             {
-                instance = FindAnyObjectByType<DbRequestManager>();
+                instance = FindObjectOfType<DbRequestManager>();
             }
             return instance;
         }
